362564ffb04cf7852d1080eb3cede71080c9921e
ljdr64 <ljdr64@users.noreply.github.com>
2023-04-15 17:51:12 +0000

    deploy: 0a35aeae9a3e639571f65a6269cd796854873e40

--- _dist_/index.js
+++ _dist_/index.js
@@ -6,6 +6,9 @@
 const cities = ['London','Paris','Tokio'];
 const apiKey = 'b3093bd1de5429955c010ac133531e04';
 const constKelvin = 273.15;
+const appNode = document.querySelector("#app");
+let searchedCityName = '';
+
 let converseKelvinCelsius = (temp) => { return Math.round(temp - constKelvin) };
 
 const url = (cityName) => {
@@ -14,13 +17,16 @@ const url = (cityName) => {
 const urlIcon = `https://openweathermap.org/img/wn/`;
 const urlIconExt = `@2x.png`;
 
-cities.map((cityName1) => {
+
+let weatherCity = (cityName1) => {
 window
 .fetch(url(cityName1))
 .then(respuesta => respuesta.json())
 .then((responseJson) => {
 
-    const cityNameL1 = cityName1.toLowerCase()
+    console.log(responseJson)
+
+    const cityNameL1 = cityName1.toLowerCase().split(' ').join('-');
 
     const cityName = document.querySelector(`.city-name-${cityNameL1}`);
     cityName.textContent = cityName1 + ', ' + responseJson.sys.country;
@@ -43,4 +49,61 @@ window
     description.textContent = responseJson.weather[0].description.toUpperCase();
 
 })
-});
+};
+
+cities.map((city) => { return weatherCity(city) });
+
+let accionSearchCity = (event) => {
+    // <div class="text-lg font-serif p-4 cards bg-blue-500 w-52 m-2.5
+    //        border-2 border-black rounded-2xl">
+    //         <h1 class="bg-gray-300 bg-white rounded-md city-name-${searchedCityName}"></h1>
+    //         <div class="pt-4 text-5xl main-temp-${searchedCityName}"></div>
+    //         <div class="card-img-${searchedCityName} w-full"></div>
+    //         <div class="text-xl main-temp-max-${searchedCityName}"></div>
+    //         <div class="text-xl main-temp-min-${searchedCityName}"></div>
+    //         <div class="description-${searchedCityName}"></div>
+    // </div>
+
+    event.preventDefault();
+
+    let searchedCityName = document.getElementById('input-search').value.toLowerCase().split(' ').join('-');
+
+        const h1 = document.createElement('h1');
+        h1.className = `bg-gray-300 bg-white rounded-md city-name-${searchedCityName}`;
+
+        const div1 = document.createElement('div');
+        div1.className = `pt-4 text-5xl main-temp-${searchedCityName}`;
+        
+        const div2 = document.createElement('div');
+        div2.className = `card-img-${searchedCityName} w-full`;
+
+        const div3 = document.createElement('div');
+        div3.className = `text-xl main-temp-max-${searchedCityName}`;        
+
+        const div4 = document.createElement('div');
+        div4.className = `text-xl main-temp-min-${searchedCityName}`;
+
+        const div5 = document.createElement('div');
+        div5.className = `description-${searchedCityName}`;
+
+        const container = document.createElement('div');
+        container.append(h1, div1, div2, div3, div4, div5);
+        container.className = 'text-lg font-serif p-4 cards bg-blue-500 w-52 m-2.5 border-2 border-black rounded-2xl';
+    
+    searchedCityName = searchedCityName[0].toUpperCase() + searchedCityName.slice(1).toLowerCase().split('-').join(' ');
+
+    fetch( url(searchedCityName) )
+        .then(response => {
+           if(response.status == 200) {            
+            appNode.insertAdjacentHTML("afterbegin", container.outerHTML);
+            weatherCity(searchedCityName);
+            } else {
+                throw ('error')
+            }
+        })
+        .catch((err) => alert(err));    
+}
+
+let formSearch = document.getElementById('form-search');
+
+formSearch.addEventListener('submit', accionSearchCity);
